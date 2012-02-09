511a204fffb762bab2a5dab256ccb3701db214e4
claudio <claudio@d82a3e6f-4ec7-4685-9f98-4b2599d2915b>
2012-02-09 17:52:16 +0000

    - Organizada de templates

--- src/Dodici/Fansworld/WebBundle/Controller/DefaultController.php
+++ src/Dodici/Fansworld/WebBundle/Controller/DefaultController.php
@@ -16,4 +16,73 @@ class DefaultController extends Controller
     {
         return array('name' => $name);
     }
+    
+	/**
+     * Sidebar controller action
+     * @Template
+     */
+    public function sidebarAction()
+    {
+    	return array();
+    }
+    
+	/**
+     * Leftbar controller action
+     * @Template
+     */
+    public function leftbarAction()
+    {
+    	$user = $this->get('security.context')->getToken()->getUser();
+    	return array('user' => $user);
+    }
+    
+	/**
+     * Rightbar controller action
+     * @Template
+     */
+    public function rightbarAction()
+    {
+    	$user = $this->get('security.context')->getToken()->getUser();
+    	return array('user' => $user);
+    }
+    
+	/**
+     * Top controller action
+     * @Template
+     */
+    public function topAction()
+    {
+    	return array();
+    }
+    
+	/**
+     * force mobile
+     * 
+     * @Route("/mobile/{value}", requirements={"value"="yes|no"}, name="force_mobile")
+     */
+    public function forcemobileAction($value)
+    {
+        $request = $this->getRequest();
+        $host = $request->getHost();
+        if (strpos($host, 'm.') === 0)
+        	$host = substr($host, 2);
+        	
+    	if ($value == 'yes') {
+    		$url = 'http://m.'.$host.$this->generateUrl('homepage');
+    	} else { 
+    		$url = 'http://'.$host.$this->generateUrl('homepage');
+    	}
+    	
+    	$response = new RedirectResponse($url);
+        $cookie = new Cookie('force'.$value.'mobile', '1', time() + (3600 * 48), '/', $host);
+		$response->headers->setCookie($cookie);
+		if ($value == 'yes') {
+			$cookie = new Cookie('forcenomobile', '0', time() + (3600 * 48), '/', $host);
+			$response->headers->setCookie($cookie);
+		} else {
+			$cookie = new Cookie('forceyesmobile', '0', time() + (3600 * 48), '/', $host);
+			$response->headers->setCookie($cookie);
+		}
+		return $response;
+    }
 }
