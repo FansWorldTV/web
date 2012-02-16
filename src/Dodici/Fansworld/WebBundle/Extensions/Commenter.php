6f3281b4c41fc50356e08a34566fc6f45231702f
claudio <claudio@d82a3e6f-4ec7-4685-9f98-4b2599d2915b>
2012-02-16 18:20:49 +0000

    - Fixes render comment - Commenter service

---
+++ src/Dodici/Fansworld/WebBundle/Extensions/Commenter.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Dodici\Fansworld\WebBundle\Extensions;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use Application\Sonata\UserBundle\Entity\User;
+use Dodici\Fansworld\WebBundle\Entity\Comment;
+use Dodici\Fansworld\WebBundle\Entity\Privacy;
+use Doctrine\ORM\EntityManager;
+
+class Commenter
+{
+	protected $request;
+	protected $em;
+
+    function __construct(EntityManager $em)
+    {
+        $this->request = Request::createFromGlobals();
+        $this->em = $em;
+    }
+
+    /**
+     * Create a comment on entity, authored by user, with text content, and privacy_type
+     * @param User $user
+     * @param $entity
+     * @param string $content
+     * @param Privacy::* $privacy_type
+     */
+    public function comment(User $user, $entity, $content, $privacy_type = Privacy::EVERYONE)
+    {
+    	$exp = explode('\\', get_class($entity));
+    	$classname = end($exp);
+    	
+    	$comment = new Comment();
+		$comment->setType(Comment::TYPE_COMMENT);
+		$comment->setAuthor($user);
+		$comment->setPrivacy($privacy_type);
+		$comment->setContent($content);
+		
+		if ($entity instanceof User) {
+			$comment->setTarget($entity);
+		} else {
+			$methodname = 'set'.$classname;
+			$comment->$methodname($entity);
+		}
+		
+    	$this->em->persist($comment);
+			
+		$this->em->flush();
+    }    
+}
