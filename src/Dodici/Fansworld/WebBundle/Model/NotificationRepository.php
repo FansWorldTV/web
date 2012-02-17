093698eb15a3e003bcf6d0f20e3dd0b621c236fe
claudio <claudio@d82a3e6f-4ec7-4685-9f98-4b2599d2915b>
2012-02-17 20:17:44 +0000

    - NotificationRepository::latest y count

--- src/Dodici/Fansworld/WebBundle/Model/NotificationRepository.php
+++ src/Dodici/Fansworld/WebBundle/Model/NotificationRepository.php
@@ -2,6 +2,8 @@
 
 namespace Dodici\Fansworld\WebBundle\Model;
 
+use Doctrine\DBAL\Types\Type;
+
 use Doctrine\ORM\EntityRepository;
 
 /**
@@ -12,4 +14,54 @@ use Doctrine\ORM\EntityRepository;
  */
 class NotificationRepository extends EntityRepository
 {
+	/**
+     * Get notifications for user
+     */
+    public function latest(\Application\Sonata\UserBundle\Entity\User $user, $readed = null, $limit = null, $offset = null)
+    {
+    	$query = $this->_em->createQuery('
+    	SELECT n
+    	FROM \Dodici\Fansworld\WebBundle\Entity\Notification n
+    	WHERE
+    	n.target = :userid AND n.active=true
+    	'
+    	. ($readed !== null ? ' AND n.readed = :readed ' : '') .
+    	'
+    	ORDER BY n.readed DESC, n.createdAt DESC
+    	'
+    	)
+    		->setParameter('userid', $user->getId(), Type::BIGINT);
+
+    	if ($readed !== null)
+		$query = $query->setParameter('readed', $readed, Type::BOOLEAN);
+    	
+    	if ($limit !== null)
+    	$query = $query->setMaxResults($limit);
+    	
+    	if ($offset !== null)
+    	$query = $query->setFirstResult($offset);
+    		
+    	return $query->getResult();
+    }
+    
+	/**
+     * Count notifications for user
+     */
+    public function countLatest(\Application\Sonata\UserBundle\Entity\User $user, $readed = null)
+    {
+    	$query = $this->_em->createQuery('
+    	SELECT COUNT(n.id)
+    	FROM \Dodici\Fansworld\WebBundle\Entity\Notification n
+    	WHERE
+    	n.target = :userid AND n.active=true
+    	'
+    	. ($readed !== null ? ' AND n.readed = :readed ' : '')
+    	)
+    		->setParameter('userid', $user->getId(), Type::BIGINT);
+    		
+    	if ($readed !== null)
+		$query = $query->setParameter('readed', $readed, Type::BOOLEAN);
+    		
+    	return (int)$query->getSingleScalarResult();
+    }
 }
