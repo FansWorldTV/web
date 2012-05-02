d2c47eca5f74ba637c10d51b42fb78b128e8c810
claudio <claudio@d82a3e6f-4ec7-4685-9f98-4b2599d2915b>
2012-05-02 19:27:52 +0000

    - Busca amigos facebook comun

--- src/Dodici/Fansworld/WebBundle/Extensions/ContactImporter.php
+++ src/Dodici/Fansworld/WebBundle/Extensions/ContactImporter.php
@@ -2,6 +2,8 @@
 
 namespace Dodici\Fansworld\WebBundle\Extensions;
 
+use Application\Sonata\UserBundle\Entity\User;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\DependencyInjection\Container;
 use Doctrine\ORM\EntityManager;
@@ -34,4 +36,41 @@ class ContactImporter
     	return $this->inviter->getMyContacts(); 
     	exit;
     }    
+    
+    /**
+     * Get friends from facebook
+     * @param Application\Sonata\UserBundle\Entity\User $user
+     * @throws \Exception
+     */
+    public function facebookFriends($user=null)
+    {
+    	if (!$user) {
+    		$user = $this->container->get('security.context')->getToken()->getUser();
+    	}
+    	if (!($user instanceof User)) throw new \Exception('Falta usuario');
+    	if (!$user->getFacebookId()) throw new \Exception('Usuario sin ID Facebook');
+    	
+    	$facebook = $this->container->get('fos_facebook.api');
+    	$friends = $facebook->api('/me/friends');
+    	if (isset($friends['data'])) $friends = $friends['data'];
+    	return $friends;
+    }
+    
+    /**
+     * Get users that are facebook friends
+     * @param Application\Sonata\UserBundle\Entity\User $user
+     * @throws \Exception
+     */
+    public function facebookFansworld($user=null)
+    {
+    	$friends = $this->facebookFriends($user);
+    	if (!$friends) throw new \Exception('Sin amigos');
+    	$ids = array();
+    	foreach ($friends as $friend) {
+    		$ids[] = $friend['id'];
+    	}
+    	$friendrepo = $this->container->get('doctrine.orm.entity_manager')->getRepository('Application\\Sonata\\UserBundle\\Entity\\User');
+    	$fwfriends = $friendrepo->findBy(array('enabled' => true, 'facebookId' => $ids));
+    	return $fwfriends;
+    }
 }
