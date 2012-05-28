531c8d85ca689ffd54869c70e70c44b99e9d04fb
nahuel <nahuel@d82a3e6f-4ec7-4685-9f98-4b2599d2915b>
2012-05-28 18:52:17 +0000

    importerrr

---
+++ src/Dodici/Fansworld/WebBundle/Controller/InviteController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Dodici\Fansworld\WebBundle\Controller;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use JMS\SecurityExtraBundle\Annotation\Secure;
+use Symfony\Component\Form\FormError;
+use Application\Sonata\MediaBundle\Entity\Media;
+use Symfony\Component\Validator\Constraints\Collection;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Application\Sonata\UserBundle\Entity\User;
+use Dodici\Fansworld\WebBundle\Extensions\ContactImporter;
+
+/**
+ * @Route("/inviter") 
+ */
+class InviteController extends SiteController
+{
+
+    /**
+     * @Route("/", name="invite_index") 
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/ajax/import", name="invite_ajaximport")
+     */
+    public function ajaxImportAction()
+    {
+        $request = $this->getRequest();
+        $mail = $request->get('mail', false);
+        $password = $request->get('password', false);
+        $provider = $request->get('provider', false);
+
+        $response = array(
+            'error' => true,
+            'contacts' => null
+        );
+
+        if ($mail && $password && $provider) {
+            $importer = $this->get('contact.importer');
+            $importer instanceof ContactImporter;
+
+            $response['contacts'] = $importer->import($mail, $password, $provider);
+            $response['error'] = false;
+        } else {
+            $response['error'] = 'Mail o contraseña invalido';
+        }
+
+        return $this->jsonResponse($response);
+    }
+
+    /**
+     * @Route ("/ajax/generate-invitation", name="invite_generateInvitation")
+     */
+    public function ajaxGenerateInvitation()
+    {
+        $request = $this->getRequest();
+        $users2bInvited = $request->get('users', null);
+        $response = null;
+
+        if ($users2bInvited) {
+            $user = $this->get('security.context')->getToken()->getUser();
+
+            $importer = $this->get('contact.importer');
+            $importer instanceof ContactImporter;
+
+
+            foreach ($users2bInvited as $user2bInvited) {
+                $inviteUrl = $importer->inviteUrl($user);
+
+                $mailer = $this->get('mailer');
+                $subject = "Invitation";
+                $html = $this->get('templating')->render('DodiciFansworldWebBundle:Invite:new_invitation.html.twig', array('url' => $inviteUrl, 'who' => $user));
+                $message = \Swift_Message::newInstance()
+                        ->setSubject($subject)
+                        ->setFrom('info@fansworld.tv')
+                        ->setTo($user2bInvited)
+                        ->setBody(trim(strip_tags($html)))
+                        ->addPart($html, 'text/html');
+                $sent = $mailer->send($message);
+
+                $response['invites'][$user2bInvited] = array(
+                    'url' => $inviteUrl,
+                    'sent' => $sent
+                );
+            }
+        }
+
+        return $this->jsonResponse($response);
+    }
+
+}
