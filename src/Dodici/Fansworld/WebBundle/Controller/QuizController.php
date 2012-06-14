3d8abc484fb550fda8aae59d229014647fe1821b
nahuel <nahuel@d82a3e6f-4ec7-4685-9f98-4b2599d2915b>
2012-06-14 14:59:59 +0000

    ticket 299

--- src/Dodici/Fansworld/WebBundle/Controller/QuizController.php
+++ src/Dodici/Fansworld/WebBundle/Controller/QuizController.php
@@ -11,6 +11,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Dodici\Fansworld\WebBundle\Controller\SiteController;
 use Dodici\Fansworld\WebBundle\Entity\Complaint;
+use Dodici\Fansworld\WebBundle\Entity\QuizQuestion;
 use Dodici\Fansworld\WebBundle\Entity\QuizAnswer;
 use Dodici\Fansworld\WebBundle\Entity\QuizOption;
 use Symfony\Component\HttpFoundation\Request;
@@ -46,25 +47,30 @@ class QuizController extends SiteController
         $quiz = $this->getRepository('QuizQuestion')->find($quizId);
         if ($quiz) {
             $author = $this->get('security.context')->getToken()->getUser();
+            $quizAlreadyResponded = $this->getRepository('QuizAnswer')->countBy(array('author' => $author->getId(), 'quizquestion' => $quiz->getId()));
+            if ($quizAlreadyResponded > 0) {
+                $response['error'] = true;
+                $response['msg'] = 'already responded';
+            } else {
+                try {
+                    $quizAnswer = new QuizAnswer();
+                    $quizAnswer->setAuthor($author);
+                    $quizAnswer->setQuizquestion($quiz);
 
-            try {
-                $quizAnswer = new QuizAnswer();
-                $quizAnswer->setAuthor($author);
-                $quizAnswer->setQuizquestion($quiz);
-
-                foreach ($quiz->getQuizoptions() as $op) {
-                    if (array_key_exists($op->getId(), $optionsVote)) {
-                        $quizAnswer->addQuizOption($op);
+                    foreach ($quiz->getQuizoptions() as $op) {
+                        if (array_key_exists($op->getId(), $optionsVote)) {
+                            $quizAnswer->addQuizOption($op);
+                        }
                     }
+
+                    $em = $this->getDoctrine()->getEntityManager();
+                    $em->persist($quizAnswer);
+                    $em->flush();
+
+                    $response['error'] = false;
+                } catch (Exception $exc) {
+                    $response['msg'] = $exc->getMessage();
                 }
-
-                $em = $this->getDoctrine()->getEntityManager();
-                $em->persist($quizAnswer);
-                $em->flush();
-
-                $response['error'] = false;
-            } catch (Exception $exc) {
-                $response['msg'] = $exc->getMessage();
             }
         } else {
             $response['msg'] = "quizid doesnt exists";
@@ -74,17 +80,33 @@ class QuizController extends SiteController
         return $this->jsonResponse($response);
     }
 
+    /**
+     * @Route("/results", name="quiz_results")
+     */
     public function seeResultsAction()
     {
         $request = $this->getRequest();
         $quizId = $request->get('quizid', false);
+
         $response = array(
-            'answers' => null
+            'answers' => null,
+            'total' => 0
         );
 
-        $quizAnswers = $this->getRepository('QuizAnswer')->findBy(array('quizquestion' => $quizId), array('createdAt' => 'desc'));
-        foreach ($quizAnswers as $answer) {
-            $response['answers'][$answer->getQuizquestion()->getId()] = $answer->getOptions();
+        $quiz = $this->getRepository('QuizQuestion')->find($quizId);
+        $quiz instanceof QuizQuestion;
+
+        foreach ($quiz->getQuizanswers() as $answer) {
+            $answer instanceof QuizAnswer;
+            foreach ($answer->getOptions() as $op) {
+                $op instanceof QuizOption;
+                if (!isset($response['answers'][$op->getId()])) {
+                    $response['answers'][$op->getId()] = 1;
+                } else {
+                    $response['answers'][$op->getId()]++;
+                }
+            }
+            $response['total']++;
         }
 
         return $this->jsonResponse($response);
