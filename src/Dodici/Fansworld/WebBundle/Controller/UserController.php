fb981429cfd0f930a630cc4a8ffde4a5076372c8
julian <julian@d82a3e6f-4ec7-4685-9f98-4b2599d2915b>
2012-07-20 20:25:43 +0000

    0000282: Perfil Usuario - 4.5.2 - Detalle Foto - maq. e impl.

--- src/Dodici/Fansworld/WebBundle/Controller/UserController.php
+++ src/Dodici/Fansworld/WebBundle/Controller/UserController.php
@@ -681,5 +681,39 @@ class UserController extends SiteController
         
         return $this->jsonResponse($response);
     }
+    
+    /**
+     * @Route("/u/{username}/photos/{id}/{slug}", name="user_photodetail")
+     * @Template
+     */
+    public function photoDetailAction($username,$id)
+    {
+    	$user = $this->getRepository('User')->findOneByUsername($username);
+    	
+    	$photoRepo = $this->getRepository('Photo');
+    	$photo = $photoRepo->findOneBy(array('id' => $id, 'active' => true));
+    	
+    	$next = $photoRepo->getNextActive($id);
+    	$prev = $photoRepo->getPrevActive($id);
+    	
+    	if (!$user) {
+    		throw new HttpException(404, "No existe el usuario");
+    	}else
+    		$this->get('visitator')->visit($photo);
+    
+    	$loggedUser = $this->get('security.context')->getToken()->getUser();
+    	$isLoggedUser = $user->getId() == $loggedUser->getId() ? true : false;
+    	 
+    	$this->securityCheck($photo);
+    
+    
+    	return array(
+    			'user' => $user,
+    			'isLoggedUser' => $isLoggedUser,
+    			'photo' => $photo,
+    			'prev' => $prev,
+    			'next' => $next
+    	);
+    }
 
 }
