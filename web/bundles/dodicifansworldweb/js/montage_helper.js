613fefbe4e7d1ad268d6522ad437866234054001
julian <julian@d82a3e6f-4ec7-4685-9f98-4b2599d2915b>
2012-08-17 18:15:50 +0000

    refacor montage

---
+++ web/bundles/dodicifansworldweb/js/montage_helper.js
@@ -0,0 +1,36 @@
+var montageHelper = {
+		
+		
+    init: function(){
+			
+    },
+    
+    defaults: {
+		minw: 150, 
+        margin: 0, 
+        liquid: true, 
+        minsize: false,
+        fillLastRow: true
+	},
+    
+    doMontage: function(container,opts){
+    	
+		var $imgs		= container.find('img').hide();
+		var totalImgs	= $imgs.length;
+		var cnt			= 0;
+		var options 		= $.extend({},montageHelper.defaults,opts);
+		
+		$imgs.each(function(i) {
+			var $img	= $(this);
+			$('<img/>').load(function() {
+				++cnt;
+				if( cnt === totalImgs ) {
+					$imgs.show();
+					container.montage(options);
+				}
+			}).attr('src',$img.attr('src'));
+		});	
+    }
+    
+    
+};
