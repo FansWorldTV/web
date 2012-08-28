d69e8f1b4cb49ab9272133b4dfff2edb10889764
julian <julian@d82a3e6f-4ec7-4685-9f98-4b2599d2915b>
2012-08-28 19:25:54 +0000

    0000398: Fix botones fan

--- web/bundles/dodicifansworldweb/js/friendship.js
+++ web/bundles/dodicifansworldweb/js/friendship.js
@@ -22,9 +22,10 @@ var friendship = {
     },
     
     add: function(){
-        $(".btn_friendship.add:not('.loading')").live('click', function(){
+        $(".btn_friendship.add:not('.loading-small')").live('click', function(e){
+        	e.stopImmediatePropagation();
             var self = $(this);
-            self.addClass('loading');
+            self.addClass('loading-small');
             
             var targetId = $(this).closest('div.addFriend').attr('targetId');
             var friendgroups = [];
@@ -36,7 +37,8 @@ var friendship = {
             ajax.addFriendAction(targetId, friendgroups, function(response){
                 if(!response.error){
                     if (response.active) {
-                        self.removeClass('add').removeClass('btn-success').addClass('remove').attr('friendshipId', response.friendship).text('Dejar de Seguir');
+                        //self.removeClass('add').removeClass('btn-success').addClass('remove').attr('friendshipId', response.friendship).text('Dejar de Seguir');
+                    	self.removeClass('add').attr('friendshipId', response.friendship).text('Ya eres fan');
                     } else {
                         self.removeClass('add').removeClass('btn-success').addClass('remove').attr('friendshipId', response.friendship).text('Cancelar Solicitud');
                     }
@@ -44,23 +46,24 @@ var friendship = {
                 }else{
                     error(response.error);
                 }
-                self.removeClass('loading');
+                self.removeClass('loading-small');
             });
         });
     },
     
     cancel: function(){
-        $(".btn_friendship.remove:not('.loading')").live('click', function(){
+        $(".btn_friendship.remove:not('.loading')").live('click', function(e){
+        	e.stopImmediatePropagation();
             var self = $(this);
             var friendshipId = $(this).attr('friendshipId');
             if(confirm('Seguro deseas dejar de seguir a este fan?')){
-                self.addClass('loading');
+                self.addClass('loading-small');
                 ajax.cancelFriendAction(friendshipId, function(response){
                     if(!response.error) {
                         window.location.reload();  
                     }else{
                         error(response.error);
-                        self.removeClass('loading');
+                        self.removeClass('loading-small');
                     }
                 });
             }
