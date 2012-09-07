b408a199fe6b0ede87f5e6e68527531d0346c4a9
andres <andres@d82a3e6f-4ec7-4685-9f98-4b2599d2915b>
2012-09-07 14:04:01 +0000

    0000364: Fix Templatehelper queue

--- web/bundles/dodicifansworldweb/js/template_helper.js
+++ web/bundles/dodicifansworldweb/js/template_helper.js
@@ -1,90 +1,114 @@
 var templateHelper = {
 
-    'getPath': function(templateId) {
+    'queue': null,
+
+    'items': {},
+
+    'init': function () {
+        "use strict";
+
+        templateHelper.queue = $.jqmq({
+            delay: -1,
+            batch: 1,
+            callback: function (templateId) {
+                var item = templateHelper.items[templateId];
+
+                $.ajax({
+                    url: templateHelper.getPath(templateId),
+                    type: 'get',
+                    dataType: "html",
+                    cache: false,  ///////////////////////////////////////////////////////////////////////////// poner en true
+                    success: function (data) {
+                        if (!data) {
+                            templateHelper.queue.next(true);
+                        }
+
+                        $.templates(templateId, data);
+                        if (typeof item.callback === "function") {
+                            item.callback();
+                        }
+
+                        templateHelper.queue.next();
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        console.error(jqXHR, textStatus, errorThrown);
+                    }
+                });
+
+            },
+            complete: function () {
+            }
+        });
+    },
+
+    'getPath': function (templateId) {
+        "use strict";
+
         var tplPart = templateId.split("-");
         return Routing.generate(appLocale + '_template_' + tplPart[0], {
             type: tplPart[1]
         });
-    }, 
-		
-    'preLoadTemplates': function(templates) {
-        $.each(templates, function(index, value) { 
-            templateHelper.loadTemplate(value);
-        });
-    }, 
-		
-    'getTemplate': function(templateId, params) {
-        if (typeof $.render[templateId] != 'undefined') {
-            return true;
-        } else {
-            return templateHelper.getTemplateString(templateId, params);
-        } 
-    }, 
-	
-    'getTemplateString': function(templateId, params) {
-        var url = templateHelper.getPath(templateId), 
-             alreadyLoaded = false;
-        	
-        return $.ajaxQueue( {
-            url: url, 
-            data: params, 
-            type: 'GET', 
-            dataType: "html", 
-            cache: true, 
-            success: function(data) {
-                $.templates(templateId, data);
-            }, 
-            error: function(jqXHR, textStatus, errorThrown) {
-                console.log(jqXHR, textStatus, errorThrown);
-            }, 
-            beforeSend : function() {
-                if (typeof $.render[templateId] != 'undefined') {
-                    alreadyLoaded = true;
-                    return true;
-                } else {
-                    alreadyLoaded = false;
-                }
-            }, 
-            abort: function() {
-                if(alreadyLoaded) {
-                    console.log('ya ta carguetti');
-                //return true;
-                }
-            }
-        });
+    },
 
-    }, 
-    
-    'loadTemplate': function(templateId) {
-        var tplString	= templateHelper.getTemplate(templateId);	
-        if(tplString == true) {
-            return true;
-        } else {
-            tplString.done(function (data) { 
-                return true;
-            });
+    'preLoadTemplates': function (templates) {
+        "use strict";
+
+        if (!templateHelper.queue) {
+            templateHelper.init();
         }
-    }, 
-		
-    'renderTemplate': function(templateId, jsonData, destino, prepend, callback) {
-        var tplString	= templateHelper.getTemplate(templateId);	
-        if(tplString == true) {
-            templateHelper.appendRenderedTemplate(templateId, jsonData, destino, prepend, callback);	
-        } else {
-            tplString.done(function (data) { 
+
+        $.each(templates, function (index, value) {
+            templateHelper.getTemplate(value);
+        });
+    },
+
+    'getTemplate': function (templateId, callback) {
+        "use strict";
+
+        if (typeof $.render[templateId] !== "undefined") {
+            if (typeof callback === "function") {
+                return callback();
+            }
+
+            return true;
+        }
+        var item = {
+            'templateId': templateId,
+            'callback': callback
+        };
+
+        if (typeof templateHelper.items[templateId] === "undefined") {
+            templateHelper.items[templateId] = item;
+            templateHelper.queue.add(templateId);
+        }
+    },
+
+    'renderTemplate': function (templateId, jsonData, destino, prepend, callback) {
+        "use strict";
+
+        try {
+            if (!templateHelper.queue) {
+                templateHelper.init();
+            }
+
+            templateHelper.getTemplate(templateId, function () {
                 templateHelper.appendRenderedTemplate(templateId, jsonData, destino, prepend, callback);
             });
+        } catch (exception) {
+            console.error(exception);
         }
-    }, 
-		
-    'appendRenderedTemplate': function(templateId, jsonData, destino, prepend, callback) {
-        if(prepend == true) {
+    },
+
+    'appendRenderedTemplate': function (templateId, jsonData, destino, prepend, callback) {
+        "use strict";
+
+        if (prepend === true) {
             $(destino).prepend($.render[templateId](jsonData));
         } else {
             $(destino).append($.render[templateId](jsonData));
         }
-        
-        if(typeof callback == 'function') {
+
+        if (typeof callback === 'function') {
             callback();
         }
     }
