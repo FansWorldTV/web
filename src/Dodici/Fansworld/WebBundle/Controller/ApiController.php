cc412750f190d3cd0fc41ff90b4301baae57a26f
claudio <claudio@d82a3e6f-4ec7-4685-9f98-4b2599d2915b>
2012-09-18 13:17:22 +0000

    - Preliminar api controller

---
+++ src/Dodici/Fansworld/WebBundle/Controller/ApiController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Dodici\Fansworld\WebBundle\Controller;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Dodici\Fansworld\WebBundle\Controller\SiteController;
+
+/**
+ * API controller
+ * REST, json
+ * 
+ * @Route("/api")
+ */
+class ApiController extends SiteController
+{
+    const TIMESTAMP_MARGIN = 120;
+    
+    /**
+     * Get server timestamp
+     * @Route("/sync", name="api_sync")
+     */
+    public function syncAction()
+    {
+        $now = new \DateTime();
+        return $this->jsonResponse($now->format('U'));
+    }
+    
+    /**
+     * test method
+     * @Route("/test", name="api_test")
+     */
+    public function testAction()
+    {
+        if ($this->hasValidSignature()) {
+            return new Response('valid signed request');
+        } else {
+            $request = $this->getRequest();
+            $key = $request->get('api_key');
+            $timestamp = $request->get('api_timestamp');
+            $signature = $request->get('api_signature');
+            $apikey = $this->getApiKeyByKey($key);
+            
+            $txt = 'invalid signed request<br>';
+            $txt .= 'key: ' . $key . '<br>';
+            $txt .= 'timestamp: ' . $timestamp . '<br>';
+            $txt .= 'signature: ' . $signature . '<br>';
+            $txt .= '<br>';
+            
+            if ($apikey) {
+                $txt .= 'expected sig: ' . $this->createSignature($key, $timestamp, $apikey->getSecret());
+            } else {
+                $txt .= 'no apikey found for key';
+            }
+            
+            return new Response($txt);
+        }
+        
+    }
+    
+    /**
+     * Does this request have a valid signature behind it?
+     */
+    private function hasValidSignature()
+    {
+        $request = $this->getRequest();
+        $key = $request->get('api_key');
+        $timestamp = $request->get('api_timestamp');
+        $signature = $request->get('api_signature');
+        
+        return $this->validateSignature($key, $timestamp, $signature);
+    }
+    
+    /**
+     * Validate a signature
+     * @param string $key
+     * @param int $timestamp
+     * @param string $signature
+     */
+    private function validateSignature($key, $timestamp, $signature)
+    {
+		$apikey = $this->getApiKeyByKey($key);
+		$now = new \DateTime();
+		$currentts = $now->format('U');
+		$tsdiff = abs($timestamp - $currentts);
+		if ($tsdiff > self::TIMESTAMP_MARGIN) throw new HttpException(400, 'Timestamp is too old');
+		
+		if (!$apikey) throw new HttpException(400, 'Bad api key');
+		if (!$signature) throw new HttpException(400, 'Bad signature');
+		
+		$sig = $this->createSignature($key, $timestamp, $apikey->getSecret());
+		
+		return ($sig == $signature);
+    }
+    
+    /**
+     * Create a signature from parameters
+     * @param string $key
+     * @param int $timestamp
+     * @param string $secret
+     */
+    private function createSignature($key, $timestamp, $secret)
+    {
+        $str = 'api_key='.$key.'&api_timestamp='.$timestamp.$secret;
+        return sha1($str);
+    }
+    
+    /**
+     * Returns Apikey entity corresponding to $key
+     * @param string $key
+     */
+    private function getApiKeyByKey($key)
+    {
+        $apikey = $this->getRepository('Apikey')->findOneBy(array('apikey' => $key));
+        return $apikey;
+    }
+}
---
+++ src/Dodici/Fansworld/WebBundle/Entity/Apikey.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Dodici\Fansworld\WebBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Dodici\Fansworld\WebBundle\Entity\Apikey
+ *
+ * @ORM\Table(name="apikey")
+ * @ORM\Entity
+ */
+class Apikey
+{
+    /**
+     * @var bigint $id
+     *
+     * @ORM\Column(name="id", type="bigint", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string $title
+     *
+     * @ORM\Column(name="title", type="string", length=250, nullable=false)
+     */
+    private $title;
+
+    /**
+     * @var string $apikey
+     *
+     * @ORM\Column(name="apikey", type="string", length=250, nullable=false, unique=true)
+     */
+    private $apikey;
+    
+    /**
+     * @var string $secret
+     *
+     * @ORM\Column(name="secret", type="string", length=250, nullable=false)
+     */
+    private $secret;
+    
+    public function __construct()
+    {
+        $this->apikey = sha1(uniqid());
+        $this->secret = sha1(uniqid()) . sha1(uniqid());
+    }
+
+    /**
+     * Get id
+     *
+     * @return bigint 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set apikey
+     *
+     * @param string $apikey
+     */
+    public function setApikey($apikey)
+    {
+        $this->apikey = $apikey;
+    }
+
+    /**
+     * Get apikey
+     *
+     * @return string 
+     */
+    public function getApikey()
+    {
+        return $this->apikey;
+    }
+
+    /**
+     * Set secret
+     *
+     * @param string $secret
+     */
+    public function setSecret($secret)
+    {
+        $this->secret = $secret;
+    }
+
+    /**
+     * Get secret
+     *
+     * @return string 
+     */
+    public function getSecret()
+    {
+        return $this->secret;
+    }
+}
