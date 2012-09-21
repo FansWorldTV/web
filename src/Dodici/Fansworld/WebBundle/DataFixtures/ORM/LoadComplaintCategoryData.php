fa76b7a631d32bf331a4f6085c167b629c0f16ca
claudio <claudio@d82a3e6f-4ec7-4685-9f98-4b2599d2915b>
2012-09-21 13:54:29 +0000

    - Complaint category fixture

---
+++ src/Dodici/Fansworld/WebBundle/DataFixtures/ORM/LoadComplaintCategoryData.php
@@ -0,0 +1,41 @@
+<?php
+namespace Dodici\Fansworld\WebBundle\DataFixtures\ORM;
+
+use Dodici\Fansworld\WebBundle\Entity\ComplaintCategory;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Symfony\Component\Yaml\Yaml;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+
+class LoadComplaintCategoryData extends AbstractFixture implements FixtureInterface
+{
+    const YAML_PATH = '../complaintcategories.yml';
+	
+	function load(ObjectManager $manager)
+    {
+        if (file_exists(__DIR__.'/'.self::YAML_PATH)) {
+	    	$loader = Yaml::parse(__DIR__.'/'.self::YAML_PATH);
+	    	
+	        foreach ($loader as $ct) {
+	        	$complaintcategory = new ComplaintCategory();
+	        	
+	        	if (is_array($ct['title'])) {
+	        		foreach ($ct['title'] as $locale => $title) {
+	        			$complaintcategory->setTranslatableLocale($locale);
+	        			$complaintcategory->setTitle($title);
+	        			$manager->persist($complaintcategory);
+	        			$manager->flush();
+	        		}
+	        	} else {
+	        		$complaintcategory->setTitle($ct['title']);
+	        		$manager->persist($complaintcategory);
+	        	}
+	        }
+	        
+	        $manager->flush();
+        } else {
+        	throw new \Exception('Fixture file does not exist');
+        }
+    }
+    
+}
--- src/Dodici/Fansworld/WebBundle/Entity/ComplaintCategory.php
+++ src/Dodici/Fansworld/WebBundle/Entity/ComplaintCategory.php
@@ -34,6 +34,18 @@ class ComplaintCategory implements Translatable
      */
     private $title;
     
+	/**
+	 * @Gedmo\Locale
+	 * Used locale to override Translation listener`s locale
+	 * this is not a mapped field of entity metadata, just a simple property
+	 */
+	private $locale;
+	
+	public function setTranslatableLocale($locale)
+	{
+	    $this->locale = $locale;
+	}
+    
     public function __toString()
     {
     	return $this->getTitle();
