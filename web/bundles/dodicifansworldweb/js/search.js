60a84487854a14811bc3e5e64523a0b77732cd54
nahuel <nahuel@d82a3e6f-4ec7-4685-9f98-4b2599d2915b>
2012-11-16 20:09:30 +0000

    primera version del endless

--- web/bundles/dodicifansworldweb/js/search.js
+++ web/bundles/dodicifansworldweb/js/search.js
@@ -5,6 +5,7 @@ search.query = null;
 search.active = 'all';
 search.addMore = false;
 
+
 search.init = function(query){
     if(query != ''){
         $("input[data-search-input]").val(query);
@@ -20,21 +21,29 @@ search.init = function(query){
     });
 };
 
-search.it = function(){
+search.it = function(seeAll, callback){
     var params = {
         'query': search.query,
         'page': search.page,
         'type': search.active
     };
+    $(".section.search"+search.active).addClass('loading');
     ajax.genericAction('search_ajaxsearch', params, function(r){
         if(r){
             var callback = function(){};
             var section = $("section.search."+search.active);
             search.addMore = r.addMore;
-            
+
+            var pointerLoop = 0;
             for(var i in r.search) {
                 var entity = r.search[i];
                 var destiny = null;
+                
+                if(pointerLoop == r.search.length){
+                    callback = function(){
+                        $(".section.search"+search.active).removeClass('loading');
+                    };
+                }
                 
                 switch(search.active){
                     case 'video':
@@ -57,6 +66,9 @@ search.it = function(){
                             $("[data-added-element]").imagesLoaded(function () {
                                 $('.am-container.photos').montage('add', $("[data-added-element]"));
                             }); 
+                            if(pointerLoop == r.search.length){
+                                $(".section.search"+search.active).removeClass('loading');
+                            }
                         }
                         break;
                     case 'event':
@@ -65,9 +77,22 @@ search.it = function(){
                         
                 }
                 templateHelper.renderTemplate('search-'+search.active, entity, destiny, false, callback);
+                pointerLoop++;
             }
             search.page++;
         }
+        
+        if(seeAll){
+            if(search.addMore && !search.endless().haveScroll()){
+                search.it(true);
+            }else if(search.endless().haveScroll() && search.addMore){
+                search.endless(function(){
+                   search.it(false); 
+                });
+            }
+        }
+        
+        callback();
     }, function(r){
         console.log(r);
     });
@@ -77,30 +102,70 @@ search.filter = function(){
     $("section.search").fadeIn().addClass('active');
     
     $(".search-home div[data-toggle] button[data-filter-type]").on('click', function(){
-        var params = {};
-        
-        params['type'] = $(this).attr('data-filter-type');
-        params['query'] = search.query;
-        
-        search.active = params['type'];
+        search.page = 2;
+        search.addMore = false;
+        search.active = $(this).attr('data-filter-type');
         
         $('[data-added-element]').remove();
-        search.page = 2;
         
-        if(params['type'] == 'all'){
+        if(search.active == 'all'){
             $("section.search").fadeIn().addClass('active');
         }else{
-            $("section.search").not('.'+params['type']).fadeOut().removeClass('active');
-            $("section.search."+params['type']).fadeIn().addClass('active');
-            
-            search.it();
+            $("section.search").not( '.' + search.active ).fadeOut('fast',function(){
+                $(this).removeClass('active');
+                search.it(true);
+            });
+            $("section.search." + search.active).fadeIn('fast',function(){
+                $(this).addClass('active');  
+            });
         }
     });
 };
 
+search.endless = function(callback){
+    var self = this;
+    
+    
+    self.callback = null;
+    
+    self.construct = function(callback){
+        self.callback = callback;
+        self.bindMyScroll();
+    };
+    
+    self.haveScroll = function(){
+        return $(document).height() != $(window).height();
+    };
+    
+    self.scrollBottom = function(){
+        return $(document).height() == ( $(window).scrollTop() + $(window).height());
+    };
+    
+    self.bindMyScroll = function(){
+        $(window).bind('scroll', function(){
+            if(self.haveScroll() && self.scrollBottom()){
+                console.log('bottommmmm', self.callback);
+                self.callback();
+            }
+        });
+    };
+    
+    self.construct(callback);
+    
+    return self;
+};
 
-search.endless = function(){
-    $(window).endlessScroll({
+function secToMinutes(sec){
+    var min = Math.floor(sec/60);
+    sec = sec % 60;
+    if(sec<10) sec = "0" + sec;
+    if(min<10) min = "0" + min;
+    return min + ":" + sec;
+}
+
+
+
+/*$(window).endlessScroll({
         fireOnce: true,
         enableScrollTop: false,
         inflowPixels: 100,
@@ -113,13 +178,4 @@ search.endless = function(){
                 search.it();
             }
         }
-    });
-};
-
-function secToMinutes(sec){
-    var min = Math.floor(sec/60);
-    sec = sec % 60;
-    if(sec<10) sec = "0" + sec;
-    if(min<10) min = "0" + min;
-    return min + ":" + sec;
-}
+    });*/
