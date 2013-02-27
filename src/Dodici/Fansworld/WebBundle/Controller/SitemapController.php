2b3108de4c1cddc0b3f48521aac4510287a04312
claudio <claudio@d82a3e6f-4ec7-4685-9f98-4b2599d2915b>
2013-02-27 19:21:35 +0000

    0000554: Sitemap (preliminar)

---
+++ src/Dodici/Fansworld/WebBundle/Controller/SitemapController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Dodici\Fansworld\WebBundle\Controller;
+
+use Dodici\Fansworld\WebBundle\Entity\Privacy;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Dodici\Fansworld\WebBundle\Controller\SiteController;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Sitemap controller.
+ */
+class SitemapController extends SiteController
+{
+    /**
+     * Render sitemap
+     * @Route("/sitemap.{_format}", name="sitemap_render", requirements={"_format" = "xml"})
+     * @Template("DodiciFansworldWebBundle:Sitemap:sitemap.xml.twig")
+     * @Cache(smaxage="36000",maxage="36000")
+     */
+    public function renderAction()
+    {
+        $teams = $this->getRepository('Team')->findBy(array('active' => true));
+        $idols = $this->getRepository('Idol')->findBy(array('active' => true));
+        $users = $this->getRepository('User')->findBy(array('restricted' => false, 'enabled' => true));
+        $videos = $this->getRepository('Video')->findBy(array('active' => true, 'privacy' => Privacy::EVERYONE));
+        $videocategories = $this->getRepository('VideoCategory')->findAll();
+        $photos = $this->getRepository('Photo')->findBy(array('active' => true, 'privacy' => Privacy::EVERYONE));
+        
+        return array(
+            'teams' => $teams, 'idols' => $idols, 'users' => $users, 'videos' => $videos, 'videocategories' => $videocategories,
+            'photos' => $photos
+        );
+    }
+
+}
