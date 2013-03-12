b6dcf1d13adf248886e0b832b535667abbc621d9
benjamin <benjamin@d82a3e6f-4ec7-4685-9f98-4b2599d2915b>
2013-03-12 14:42:32 +0000

    cleanup

--- web/bundles/dodicifansworldweb/js/home.js
+++ web/bundles/dodicifansworldweb/js/home.js
@@ -8,22 +8,10 @@ home.init = function(){
     if($("section").hasClass('home-content')){
         if(isLoggedIn){
             home.filterSection.activityFeed();
-            /*
-            home.loadSection.activityFeed({}, function(){
-                endless.init(1, function(){
-                    var lastDate = $('section.home-content .content-container[data-type-tab="activityFeed"] .elements .element').last().attr('data-element-date');
-                    home.loadSection.activityFeed({
-                        'date': lastDate,
-                        'filters': $("section.content-container[data-type-tab='activityFeed'] .tags .pull-left span.label.active").attr('data-feed-filter')
-                    });
-                });
-            });
-            */
             home.loadSection.activityFeed();
-        }else{
+        } else {
             home.loadSection.enjoy();
         }
-        console.log("calling toggleTabs")
         home.toggleTabs();
     }
 };
@@ -67,30 +55,6 @@ home.toggleTabs = function(){
             }
         }
 
-        /*
-        switch(typeTab){
-            case 'activityFeed':
-                endless.init(1, function(){
-                    var lastDate = $('section.home-content .content-container[data-type-tab="activityFeed"] #elements .post').last().attr('data-element-date');
-                    home.loadSection.activityFeed({
-                        'date': lastDate,
-                        'filters': $("section.content-container[data-type-tab='activityFeed'] .tags .pull-left span.label.active").attr('data-feed-filter')
-                    });
-                });
-                break;
-            case 'popularFeed':
-                endless.init(1, function(){
-                    var lastDate = $('section.home-content .content-container[data-type-tab="popularFeed"] #elements .post').last().attr('data-element-date');
-                    home.loadSection.popularFeed({
-                        'date': lastDate
-                    });
-                });
-                break;
-            default:
-                endless.stop();
-        }
-        */
-
         $('section.home-content .legend[data-type-tab="' + typeTab + '"]').show();
         $("section.home-content .content-container[data-type-tab='" + typeTab + "']").show();
 
@@ -128,22 +92,6 @@ home.loadedSection = {
     'activityFeed': false,
     'popularFeed': false
 };
-
-home.getMaxSections = function(container) {
-    var $container = $(container)
-    if($container.width() <= 600) {
-        var cells = 1;
-    } else if ($container.width() <= 800) {
-        var cells = 2;
-    } else if ($container.width() <= 1000) {
-        var cells = 3;
-    } else if ($container.width() <= 1200) {
-        var cells = 4;
-    } else if ($container.width() > 1200) {
-       var cells = 5;
-    }
-    return cells;
-}
 
 home.loadSection.enjoy = function(params){
 
@@ -212,130 +160,6 @@ home.loadSection.enjoy = function(params){
     };
     return;
 }
-
-home.loadSection.enjoy22 = function(){
-
-    var toAppendTrending = $('section.home-content .content-container[data-type-tab="enjoy"] .tags .pull-left');
-    var toAppendVideos = $('section.home-content .content-container[data-type-tab="enjoy"] .elements');
-
-    var $contentContainer = $('section.home-content .content-container[data-type-tab="enjoy"]');
-    var $container = $contentContainer.find('#elements');
-
-
-    if(!$contentContainer.hasClass('isIso'))
-    {
-        $container.isotope({
-            itemSelector : '.item',
-            resizable: false, // disable normal resizing
-            masonry: {
-                columnWidth: ($container.width() / home.getMaxSections($container)),
-            }
-        });
-    }
-    $(window).smartresize(function(){
-        console.log($container.width())
-      if($container.width() <= 600) {
-        var cells = 1;
-      } else if ($container.width() <= 800) {
-        var cells = 2;
-      } else if ($container.width() <= 1000) {
-        var cells = 3;
-      } else if ($container.width() <= 1200) {
-        var cells = 4;
-      } else if ($container.width() > 1200) {
-        var cells = 5;
-      }
-      $container.find('.item').each(function(i, item){
-        var $this = $(this);
-        $this.css('width', ((100 / cells) - 2) + "%") // 2% margen entre los elementos
-      });
-      $container.isotope({
-        // update columnWidth to a percentage of container width
-        masonry: { columnWidth: $container.width() / cells }
-      });
-      $container.isotope('reLayout');
-    });
-    $contentContainer.addClass('isIso');
-    toAppendVideos.addClass('loading');
-
-    ajax.genericAction('home_ajaxenjoy', {}, function(r){
-
-        var dummy = $('<div class="dummy"></div>');
-        var $container = $contentContainer.find('#elements');
-
-        for(var i in r.trending){
-            var tag = r.trending[i];
-
-            var elementToAppend = $('<span class="label"></span>');
-
-            elementToAppend.html(tag.title)
-            .attr('data-tag-id', tag.id)
-            .attr('data-tag-slug', tag.slug);
-
-            toAppendTrending.append(elementToAppend);
-        }
-
-        for(var i in r.videos){
-            var video = r.videos[i];
-
-            var loop = parseInt(i);
-            loop++;
-            if(r.videos.length == loop)  {
-                var callback = function(){
-                    toAppendVideos.removeClass('loading')
-                    home.loadedSection.enjoy = true;
-                    var post = dummy.find('.post');
-                      if($container.width() <= 600) {
-                        var cells = 1;
-                      } else if ($container.width() <= 800) {
-                        var cells = 2;
-                      } else if ($container.width() <= 1000) {
-                        var cells = 3;
-                      } else if ($container.width() <= 1200) {
-                        var cells = 4;
-                      } else if ($container.width() > 1200) {
-                        var cells = 5;
-                      }
-                    $.each(post, function(i, item) {
-                        var $this = $(item);
-                        $this.css('width', ((100 / cells) - 2) + "%")
-                        $this.find('.image').load(function() {
-                            $container.isotope('reLayout');
-                        })
-                        $container.append($(item)).isotope('appended', $(item));
-                       });
-                        $container.isotope('reLayout');
-                    };
-            }else{
-                var callback = function(){};
-            }
-
-            var href = Routing.generate(appLocale + '_video_show', {
-                'id': video.id,
-                'slug': video.slug
-            });
-
-            var jsonData = {
-                'type': 'video',
-                'date': video.createdAt,
-                'href': href,
-                'image': video.image,
-                'slug': video.slug,
-                'title': video.title
-            };
-
-            if(video.author != null){
-                jsonData['author'] = video.author.username;
-                jsonData['authorHref'] = video.author.url;
-                jsonData['authorImage'] = video.author.image;
-            }
-            templateHelper.renderTemplate('general-column_element', jsonData, dummy, false, callback);
-
-        }
-    }, function(e){
-        error(e);
-    });
-};
 
 home.loadSection.connect = function(){
     var toAppendElements = $('section.home-content .content-container[data-type-tab="connect"] .fans-list');
