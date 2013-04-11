7d9278be9f4720c3cb2eba78e69d1a450789a73d
ArrayDude <arraydude@gmail.com>
2013-04-11 14:49:59 -0300

    Chequea que estes linkeado a la red social

--- web/bundles/dodicifansworldweb/js/inviter.js
+++ web/bundles/dodicifansworldweb/js/inviter.js
@@ -7,9 +7,14 @@ inviter.url.short = null;
 inviter.tabs = {};
 inviter.tabs.selected = null
 
+inviter.linkfacebook = false;
+inviter.linktwitter = false;
 
-inviter.init = function(url) {
+
+inviter.init = function(url, linkfacebook, linktwitter) {
     inviter.url.long = url;
+    inviter.linkfacebook = linkfacebook;
+    inviter.linktwitter = linktwitter;
 
     $.ajax('https://www.googleapis.com/urlshortener/v1/url', {
         'async': false,
@@ -53,96 +58,101 @@ inviter.tabs.facebook = function() {
     $container.find('.fan-friends ul').html('');
     $container.find('.invite-friends ul').html('');
 
-    FB.getLoginStatus(function(response) {
-        if (response.status === 'connected') {
+    if (inviter.linkfacebook) {
+        FB.getLoginStatus(function(response) {
+            if (response.status === 'connected') {
 
-            $container.removeClass('loading');
-            $container.find('.is-logged').show();
-            var $inviteBlock = $container.find('.invite-block');
-            $inviteBlock.find('.fan-friends').addClass('loading');
-            $inviteBlock.find('.invite-friends').addClass('loading');
+                $container.removeClass('loading');
+                $container.find('.is-logged').show();
+                var $inviteBlock = $container.find('.invite-block');
+                $inviteBlock.find('.fan-friends').addClass('loading');
+                $inviteBlock.find('.invite-friends').addClass('loading');
 
-            ajax.genericAction({
-                route: 'facebook_commonfriends',
-                params: {},
-                callback: function(response) {
-                    $.each(response.friends, function() {
-                        this.canFriend = true;
-                        $.when(templateHelper.htmlTemplate('fans-invite_element', this))
-                                .then(function(htmlTemplate) {
-                            $(htmlTemplate).appendTo($inviteBlock.find('.fan-friends ul'));
-                        })
+                ajax.genericAction({
+                    route: 'facebook_commonfriends',
+                    params: {},
+                    callback: function(response) {
+                        $.each(response.friends, function() {
+                            this.canFriend = true;
+                            $.when(templateHelper.htmlTemplate('fans-invite_element', this))
+                                    .then(function(htmlTemplate) {
+                                $(htmlTemplate).appendTo($inviteBlock.find('.fan-friends ul'));
+                            })
+                        });
+
+                        $inviteBlock.find('.fan-friends').removeClass('loading');
+                    },
+                    errorCallback: function(errorResponse) {
+                        console.log('Error');
+                        console.log(errorResponse);
+                        $inviteBlock.find('.fan-friends').removeClass('loading');
+                        error('Error');
+                    }
+                });
+
+                ajax.genericAction({
+                    route: 'facebook_notcommonfriends',
+                    params: {},
+                    callback: function(response) {
+                        $.each(response.friends, function() {
+                            var location = null;
+
+                            if (typeof(this.hometown) != 'undefined') {
+                                location = this.hometown.name;
+                            } else {
+                                location = '';
+                            }
+
+                            var jsonData = {
+                                'id': this.id,
+                                'title': this.name,
+                                'image': 'https://graph.facebook.com/' + this.id + '/picture?type=square',
+                                'canInvite': true,
+                                'canFriend': false,
+                                'location': location
+                            };
+
+                            $.when(templateHelper.htmlTemplate('fans-invite_element', jsonData))
+                                    .then(function(htmlTemplate) {
+                                $(htmlTemplate).appendTo($inviteBlock.find('.invite-friends ul'));
+                            })
+                        });
+
+                        $inviteBlock.find('.invite-friends').removeClass('loading');
+                    },
+                    errorCallback: function(errorResponse) {
+                        console.log('Error');
+                        console.log(errorResponse);
+                        $inviteBlock.find('.invite-friends').removeClass('loading');
+                    }
+                });
+
+                $("button.submit-invite").click(function() {
+                    var friendsToInvite = [];
+
+                    $inviteBlock.find('.invite-checkbox.active').each(function(index, el) {
+                        friendsToInvite.push($(this).attr('data-invite-user'));
                     });
 
-                    $inviteBlock.find('.fan-friends').removeClass('loading');
-                },
-                errorCallback: function(errorResponse) {
-                    console.log('Error');
-                    console.log(errorResponse);
-                    $inviteBlock.find('.fan-friends').removeClass('loading');
-                    error('Error');
-                }
-            });
+                    $.modalPopup('close');
 
-            ajax.genericAction({
-                route: 'facebook_notcommonfriends',
-                params: {},
-                callback: function(response) {
-                    $.each(response.friends, function() {
-                        var location = null;
-
-                        if (typeof(this.hometown) != 'undefined') {
-                            location = this.hometown.name;
-                        } else {
-                            location = '';
-                        }
-
-                        var jsonData = {
-                            'id': this.id,
-                            'title': this.name,
-                            'image': 'https://graph.facebook.com/' + this.id + '/picture?type=square',
-                            'canInvite': true,
-                            'canFriend': false,
-                            'location': location
-                        };
-
-                        $.when(templateHelper.htmlTemplate('fans-invite_element', jsonData))
-                                .then(function(htmlTemplate) {
-                            $(htmlTemplate).appendTo($inviteBlock.find('.invite-friends ul'));
-                        })
+                    FB.ui({method: 'apprequests',
+                        message: 'My Great Request',
+                        to: friendsToInvite
                     });
-
-                    $inviteBlock.find('.invite-friends').removeClass('loading');
-                },
-                errorCallback: function(errorResponse) {
-                    console.log('Error');
-                    console.log(errorResponse);
-                    $inviteBlock.find('.invite-friends').removeClass('loading');
-                }
-            });
-
-            $("button.submit-invite").click(function() {
-                var friendsToInvite = [];
-                
-                $inviteBlock.find('.invite-checkbox.active').each(function(index, el) {
-                    friendsToInvite.push($(this).attr('data-invite-user'));
                 });
-                
-                $.modalPopup('close');
-                
-                FB.ui({method: 'apprequests',
-                    message: 'My Great Request',
-                    to: friendsToInvite
-                });
-            });
 
-        } else {
-            $container.removeClass('loading');
-            $container.find('.not-logged').show();
-        }
+            } else {
+                $container.removeClass('loading');
+                $container.find('.not-logged').show();
+            }
 
-        return false;
-    });
+            return false;
+        });
+    } else {
+        $container.removeClass('loading');
+        $container.find('.not-logged').show();
+    }
 };
 
 inviter.tabs.email = function() {
@@ -168,4 +178,9 @@ inviter.tabs.email = function() {
 inviter.tabs.twitter = function() {
     var $container = $("div.content-modal[data-type ='twitter']");
     $container.find('textarea').html('¡ Los invito a Fansworld.TV ! ' + inviter.url.short);
+    if(inviter.linktwitter){
+        $container.find('.is-logged').show();
+    }else{
+        $container.find('.not-logged').show();
+    }
 };
