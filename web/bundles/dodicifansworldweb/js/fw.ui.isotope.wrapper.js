229fabc8680ef066c4ac117a0c1f2405be0dae84
Benjamin <benjius@desktop.(none)>
2013-04-18 12:06:33 -0300

    [issue #63] quita log del wrapper que esta demas

--- web/bundles/dodicifansworldweb/js/fw.ui.isotope.wrapper.js
+++ web/bundles/dodicifansworldweb/js/fw.ui.isotope.wrapper.js
@@ -193,7 +193,6 @@ $(document).ready(function () {
 			return deferred.promise();
 		},
 		normalizeData: function(data) {
-			console.log(data)
 			var href = Routing.generate(appLocale + '_' + data.type + '_show', {
 				'id': data.id,
 				'slug': data.slug
