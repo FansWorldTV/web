dbca1b0e862a174bf99f3e558dfc03b0fdad0ea1
Juan Sanzone <js@fansworld.tv>
2013-04-25 14:20:09 -0300

    Issue #86 Perfil Idolo - Videos - calidad thumbs

--- src/Dodici/Fansworld/WebBundle/Controller/VideoController.php
+++ src/Dodici/Fansworld/WebBundle/Controller/VideoController.php
@@ -426,7 +426,7 @@ class VideoController extends SiteController
         $response['addMore'] = $countAll > self::cantVideos ? true : false;
 
         foreach ($videos as $video) {
-            $response['elements'][] = $this->get('serializer')->values($video);
+            $response['elements'][] = $this->get('serializer')->values($video, 'big');
             /*
             array(
                 'id' => $video->getId(),
@@ -501,7 +501,7 @@ class VideoController extends SiteController
         $response['elements'] = array();
 
         foreach ($videos as $video) {
-            $response['elements'][] = $this->get('serializer')->values($video);
+            $response['elements'][] = $this->get('serializer')->values($video , 'big');
             /*
             array(
                 'id' => $video->getId(),
@@ -563,7 +563,7 @@ class VideoController extends SiteController
         $response['elements'] = array();
 
         foreach ($videos as $video) {
-            $response['elements'][] = $this->get('serializer')->values($video);
+            $response['elements'][] = $this->get('serializer')->values($video, 'big');
             /*
                 array(
                     'id' => $video->getId(),
