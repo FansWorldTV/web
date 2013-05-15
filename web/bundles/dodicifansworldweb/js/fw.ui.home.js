5c98b0ca4aa8e9b9cbef4ad0915f63a25254033a
maggiben <pirulo123>
2013-05-15 12:03:37 -0300

    latest javascript

--- web/bundles/dodicifansworldweb/js/fw.ui.home.js
+++ web/bundles/dodicifansworldweb/js/fw.ui.home.js
@@ -20,6 +20,7 @@
 /*jslint devel: true */                         /* Assume console, alert, ... */
 /*jslint windows: true */               /* Assume window object (for browsers)*/
 
+
 /*******************************************************************************
  * Class dependencies:                                                         *
  *      jquery > 1.8.3                                                         *
@@ -37,7 +38,9 @@ $(document).ready(function () {
     "use strict";
     var pluginName = "fwHomeGallery";
     var defaults = {
-        style: {
+        videoCategory: null,
+        videoFeed: Routing.generate(appLocale + '_home_ajaxfilter'),
+        imteStyle: {
             'width': '16%',
             'height': '160px',
             'margin-top': '5px',
@@ -64,116 +67,47 @@ $(document).ready(function () {
             self.bind("destroyed", $.proxy(that.teardown, that));
             self.addClass(that._name);
 
-            that.appendVideos(2)
-
-            return;
-            // initialize Isotope
-            self.isotope({
-                // options...
-                itemSelector: '.video',
-                resizable: false, // disable normal resizing
-                // set columnWidth to a percentage of container width
-                masonry: { columnWidth: self.width() / 6 }
-            });
-
-            var $container = $('section.highlights');
-            $(that.options.itemSelector).css(that.options.style);
-
-            $(that.options.itemSelector + '.double').css({
-                'width': '32.5%',
-                'height': '332px'
-            });
-            $(that.options.itemSelector + ' img').css({
-                'width': '100%',
-                'height': '100%'
-            });
-            // initialize Isotope
-            $container.isotope({
-                // options...
-                itemSelector: '.video',
-                resizable: false, // disable normal resizing
-                // set columnWidth to a percentage of container width
-                masonry: { columnWidth: $container.width() / 6 }
-            });
-
-            // update columnWidth on window resize
-            $(window).smartresize(function(){
-                $container.isotope({
-                    // update columnWidth to a percentage of container width
-                    masonry: { columnWidth: $container.width() / 6 }
-                });
-            });
-
-            return;
-            // Attach to window resize event
-//            $(window).smartresize(function(){
-//                var cells = that.getMaxSections($container);
-//                self.find(that.options.itemSelector).each(function(i, item){
-//                    var $this = $(this);
-//                    $this.css('width', ((100 / cells) - 1) + "%");
-//                });
-//                $container.isotope({
-//                    // update columnWidth to a percentage of container width
-//                    masonry: { columnWidth: $container.width() / cells }
-//                });
-//                $container.isotope('reLayout');
-//            });
-
-            // update columnWidth on window resize
-//            $(window).smartresize(function(){
-//                $container.isotope({
-//                    // update columnWidth to a percentage of container width
-//                    masonry: { columnWidth: self.width() / 6 }
-//                });
-//            });
+            return true;
         },
-        appendVideos: function(videoCategory) {
+        makePackery: function(videoCategory) {
             var that = this;
             var i = 0;
-            // ajax.genericAction('home_ajaxfilter', {paginate:{'page':1, 'block':'popular','vc': 6}}, function(r){console.log(r);});
-            // $.ajax({url: Routing.generate('es_home_ajaxfilter'), data: {'vc': 1}}).then(function(r){console.log(r)})
+            var cnt = 0;
+            var container = document.querySelector('section.highlights');
+            var packery = new Packery(container, {
+                itemSelector: '.video',
+                gutter: ".gutter-sizer",
+                columnWidth: ".grid-sizer"
+            });
+
             var feed = Routing.generate(appLocale + '_home_ajaxfilter');
-            $.ajax({url: feed, data: {'vc': videoCategory}}).then(function(response){
+
+            $.ajax({url: that.options.videoFeed, data: {'vc': videoCategory}}).then(function(response){
                 for(i in response.highlighted) {
                     if (response.highlighted.hasOwnProperty(i)) {
-                        //console.log(response.highlighted[i]);
                         var video = response.highlighted[i];
-                        var thumb = document.createElement('article');
-                        var image = document.createElement('img');
-                        //thumb.className('video');
-                        $(that.element).append('<article class="video"><img src="' + video.image + '" /></article>');
+                        $.when(templateHelper.htmlTemplate('video-home_element', video))
+                        .then(function(response){
+                            var $thumb = $(response).clone();
+                            $thumb.addClass('video');
+                            if(cnt === 1) {
+                                $thumb.addClass('double');
+                            }
+                            cnt += 1;
+
+                            $('section.highlights').append($thumb);
+                            packery.appended($thumb);
+                            packery.layout();
+                        });
                     }
                 }
-                $(that.element).append()
             });
-        },
-        getMaxSections: function(container) {
-            var that = this;
-            var $container = $(that.element);
-            var cells = 0;
-            var width = parseInt($container.width(), 10);
-
-            if(width <= 600) {
-                cells = 1;
-            } else if (width <= 800) {
-                cells = 1;
-            } else if (width <= 1000) {
-                cells = 2;
-            } else if (width <= 1200) {
-                cells = 3;
-            } else if (width <= 1200) {
-                cells = 4;
-            } else if (width <= 1600) {
-                cells = 4;
-            } else if (width > 1600) {
-                cells = 5;
-            }
-            return cells;
         },
         destroy: function() {
             var that = this;
             $(that.element).unbind("destroyed", that.teardown);
             that.teardown();
+            return true;
         },
         teardown: function() {
             var that = this;
@@ -181,6 +115,7 @@ $(document).ready(function () {
             $(that.element).removeClass(that._name);
             that.unbind();
             that.element = null;
+            return that.element;
         },
         bind: function() { },
         unbind: function() { }
@@ -310,10 +245,7 @@ $(document).ready(function () {
                     $.when(templateHelper.htmlTemplate('video-home_element', video))
                         .then(function(response){
                             $thumb = $(response).clone();
-
-
-                            //container.append($thumb);
-                            $container.append($thumb);;
+                            $container.append($thumb);
                         });
                 }
             }
@@ -363,13 +295,11 @@ $(document).ready(function () {
                 if (response.highlighted.hasOwnProperty(i)) {
                     var video = response.highlighted[i];
 
-                    console.log(video)
 
                     var thumb = document.createElement('article');
                     thumb.classList.add('video');
 
                     var image = document.createElement('img');
-                    //thumb.className('video');
 
                     $thumb = $('<article class="video"><img src="' + video.image + '" title="' + video.title + '"/></article>');
 
@@ -388,7 +318,6 @@ $(document).ready(function () {
                         });
                         cnt += 1;
 
-                        //container.append($thumb);
                         $('section.highlights').append($thumb);
                         packery.appended($thumb);
                         packery.layout();
@@ -398,8 +327,6 @@ $(document).ready(function () {
         });
     }
 
-    console.log("home javascript")
-    //appendVideos(8);
     var videoCategory = $('.filter-home').find('.active').attr('data-category-id');
     makePackery(8);
     appendFollowed(videoCategory);
