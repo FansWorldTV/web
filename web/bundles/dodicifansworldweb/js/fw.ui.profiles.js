1ec878a5b2253ea458d97edd4f6f84ac8f757a3f
arraydude <arraydude@gmail.com>
2013-06-10 14:58:14 -0300

    Splash profiles home blureado por CSS3

--- web/bundles/dodicifansworldweb/js/fw.ui.profiles.js
+++ web/bundles/dodicifansworldweb/js/fw.ui.profiles.js
@@ -183,14 +183,8 @@ $(document).ready(function () {
                         if (response.profiles.hasOwnProperty(i)) {
                             var profile = response.profiles[i];
                             if(profile.highlight) {
-                                if(profile.type === 'team') {
-                                    profile.image = profile.splash_double;
-                                } else {
-                                    profile.image = profile.image_double;
-                                }
-                            }
-                            if(profile.type === 'team') {
-                                profile.image = profile.splash;
+                                profile.image = profile.image_double;
+                                profile.splash = profile.splash_double;
                             }
                             console.log(profile);
                             $.when(templateHelper.htmlTemplate('profile-home_element', profile))
