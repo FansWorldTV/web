10503aa9347fce9ed42251bd99a6810229a9518e
Juan Sanzone <js@fansworld.tv>
2013-06-26 10:56:41 -0300

    Inform if user already report entity #307

--- src/Dodici/Fansworld/WebBundle/Controller/ApiV1/ComplaintController.php
+++ src/Dodici/Fansworld/WebBundle/Controller/ApiV1/ComplaintController.php
@@ -24,7 +24,7 @@ class ComplaintController extends BaseController
 {
     /**
      * [signed] List
-     * 
+     *
      * @Route("/report/{entityType}/{entityId}", name= "api_v1_complaint")
      * @Method({"POST"})
      *
@@ -44,16 +44,22 @@ class ComplaintController extends BaseController
             if ($this->hasValidSignature()) {
                 $request = $this->getRequest();
                 if (!in_array($entityType, array('video','photo','comment'))) throw new HttpException(401, 'Invalid entity type');
-                
+
                 $userid = $request->get('user_id');
+                // $user = $this->getRepository('User')->find($userid);
                 $user = $this->checkUserToken($userid, $request->get('user_token'));
+
+                $report = $this->getRepository('Complaint')->findOneBy(array($entityType => $entityId, 'author' => $userid));
+                if ($report) throw new HttpException(420, 'User already reported this ' . $entityType);
 
                 $categoryId = $request->get('category');
                 $comment = $request->get('comment');
 
                 $entity = $this->getRepository($entityType)->find($entityId);
+                if (!$entity) throw new HttpException(401, 'Entity not found');
 
                 $category = $this->getRepository('ComplaintCategory')->find($categoryId);
+                if (!$category) throw new HttpException(401, 'Category not found');
 
                 $complaint = new Complaint();
                 $complaint->setAuthor($user);
