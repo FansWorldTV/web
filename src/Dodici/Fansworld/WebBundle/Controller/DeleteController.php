ca08cfef722315a8e3468e3f83f584b994f55436
anaibol <jam@fansworld.tv>
2013-08-08 15:56:25 -0300

    Fixes #392 Eliminar foto - Despues de haber eliminado una foto, cuando abro en album, esta presente aun.

--- src/Dodici/Fansworld/WebBundle/Controller/DeleteController.php
+++ src/Dodici/Fansworld/WebBundle/Controller/DeleteController.php
@@ -29,54 +29,54 @@ class DeleteController extends SiteController
     public function ajaxDeleteAction()
     {
         try {
-	    	$request = $this->getRequest();
-	    	$id = intval($request->get('id'));
-	        $type = $request->get('type');
-	        $translator = $this->get('translator');
-	        $appstate = $this->get('appstate');
-	        
-	        if (!in_array($type, array('photo','video','album','forumpost','comment')))
-	        throw new \Exception('Invalid type');
-	        
-	        $repo = $this->getRepository($type);
-	        $entity = $repo->find($id);
-	        $user = $this->getUser();
-	        $em = $this->getDoctrine()->getEntityManager();
-	        $redirect = null;
-	        
-	        if (!$entity->getActive()) throw new \Exception('Entity already deleted'); 
-	        
-	        if ($appstate->canDelete($entity)) {
-	        	$entity->setActive(false);
-	        	$em->persist($entity);
-	        	$em->flush();
-	        	
-	        	$message = $translator->trans('You have deleted') . ' ' . (((string)$entity) ? ('"' . (string)$entity.'"') : $translator->trans('the item'));
-	        	
-	        	switch ($type) {
-	        		//case 'photo': $redirect = $this->generateUrl('album_show', array('id' => $entity->getAlbum()->getId(), 'slug' => $entity->getAlbum()->getId())); break;
-                    case 'photo': $redirect = $this->generateUrl('user_photos', array('username' => $user->getUsername())); break;
-	        		case 'video': $redirect = $this->generateUrl('user_videos', array('username' => $user->getUsername())); break;
-	        		case 'album': $redirect = $this->generateUrl('user_listalbums', array('username' => $user->getUsername())); break;
-	        	}
-	        	
-	        } else {
-	        	if (!($user instanceof User)) {
-	        		throw new \Exception('User not logged in');
-	        	} else {
-	        		throw new \Exception('Error deleting entity');
-	        	}
-	        }
-	
-	        $response = new Response(json_encode(array(
-	        	'deleted' => true,
-	        	'message' => $message,
-	        	'redirect' => $redirect
-	        )));
-	        $response->headers->set('Content-Type', 'application/json');
-	        return $response;
+        $request = $this->getRequest();
+        $id = intval($request->get('id'));
+          $type = $request->get('type');
+          $translator = $this->get('translator');
+          $appstate = $this->get('appstate');
+          
+          if (!in_array($type, array('photo','video','album','forumpost','comment')))
+          throw new \Exception('Invalid type');
+          
+          $repo = $this->getRepository($type);
+          $entity = $repo->find($id);
+          $user = $this->getUser();
+          $em = $this->getDoctrine()->getEntityManager();
+          $redirect = null;
+          
+          if (!$entity->getActive()) throw new \Exception('Entity already deleted'); 
+          
+          if ($appstate->canDelete($entity)) {
+            $entity->setActive(false);
+            $em->persist($entity);
+            $em->flush();
+            
+            $message = $translator->trans('You have deleted') . ' ' . (((string)$entity) ? ('"' . (string)$entity.'"') : $translator->trans('the item'));
+            
+            switch ($type) {
+              //case 'photo': $redirect = $this->generateUrl('album_show', array('id' => $entity->getAlbum()->getId(), 'slug' => $entity->getAlbum()->getId())); break;
+              case 'photo': $redirect = $this->generateUrl('user_photos', array('username' => $user->getUsername())); break;
+              case 'video': $redirect = $this->generateUrl('user_videos', array('username' => $user->getUsername())); break;
+              case 'album': $redirect = $this->generateUrl('user_listalbums', array('username' => $user->getUsername())); break;
+            }
+            
+          } else {
+            if (!($user instanceof User)) {
+              throw new \Exception('User not logged in');
+            } else {
+              throw new \Exception('Error deleting entity');
+            }
+          }
+  
+          $response = new Response(json_encode(array(
+            'deleted' => true,
+            'message' => $message,
+            'redirect' => $redirect
+          )));
+          $response->headers->set('Content-Type', 'application/json');
+          return $response;
         } catch (\Exception $e) {
-        	return new Response($e->getMessage(), 400);
+          return new Response($e->getMessage(), 400);
         }
     }
 
