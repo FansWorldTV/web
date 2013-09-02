2e8c03692c40c2808e69d258899cda9529916e05
Juan <juan.sanzone@gmail.com>
2013-09-02 10:05:22 -0300

    revert test

--- src/Dodici/Fansworld/WebBundle/Controller/HomeController.php
+++ src/Dodici/Fansworld/WebBundle/Controller/HomeController.php
@@ -54,8 +54,6 @@ class HomeController extends SiteController
             $response['followed'] = $videoRepo->follow($user, $defaultGenre, $defaultCategory, self::LIMIT_VIDEO, 0);
         }
 
-        echo "test"; exit();
-
         return $response;
     }
 
